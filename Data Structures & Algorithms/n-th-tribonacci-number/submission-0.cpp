afd56f53c62175486689750adb42dfac286c3a98
Gunneet Singh <singhgunneet27@gmail.com>
2026-08-14 01:32:31 +0530

    Add: n-th-tribonacci-number - submission-0

---
+++ Data Structures & Algorithms/n-th-tribonacci-number/submission-0.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int tribonacci(int n) {
+        int t0 = 0;
+        if(n==0) return 0;
+        int t1 = 1;
+        if(n==1) return 1;
+        int t2 = 1;
+        if(n==2) return 1;
+        int tn ;
+        for(int i = 3 ; i <= n ; i++){
+            tn = t0 + t1 + t2;
+            t0 = t1;
+            t1 = t2;
+            t2 = tn;
+        }
+        return tn;
+    }
+};
